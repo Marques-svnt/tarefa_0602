500a4232c29b44f5f75434a2c42ba381e96dd497
marques-svnt <marques.svnt.002@gmail.com>
2025-02-08 15:35:07 -0300

    Adição de printf nas posições do servo motor e quando a direção alterna

--- main.c
+++ main.c
@@ -17,12 +17,14 @@ int main()
     printf("Clock periférico: %u Hz\n", clock_freq);
 
     // Posiciona em 180 graus (~2400 us)
+    printf("Posicionando o servo motor no ângulo 180º\n");
     controle(duty_cycle_2400, 5000);
 
     // Posiciona em 90 graus (~1470 us)
+    printf("Posicionando o servo motor no ângulo 90º\n");
     controle(duty_cycle_1470, 5000);
 
-    // Posiciona em 0 graus (~500 us)
+    printf("Posicionando o servo motor no ângulo 0º\n");
     controle(duty_cycle_500, 5000);
 
     // Movimentação suave entre 0 e 180 graus
@@ -32,11 +34,21 @@ int main()
         for (int duty = duty_cycle_500; duty <= duty_cycle_2400; duty += (5 * WRAP_PERIOD) / 20000)
         {
             controle(duty, 10);
+
+            if (duty == duty_cycle_2400)
+            {
+                printf("Alternando direção\n");
+            }
         }
         // Decremento suave do duty cycle (~5us)
         for (int duty = duty_cycle_2400; duty >= duty_cycle_500; duty -= (5 * WRAP_PERIOD) / 20000)
         {
             controle(duty, 10);
+
+            if (duty == duty_cycle_500)
+            {
+                printf("Alternando direção\n");
+            }
         }
     }
 }
